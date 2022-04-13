297a89ab85eeb1c63998654e9eebaa4e78a813c7
deekep <MVSein@stud.kantiana.ru>
2022-04-13 16:12:34 +0200

    add classes and lib

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
+#include <BMP.hpp>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    try
+    {
+        mt::images::BMP test_bmp(200, 150);
+        test_bmp.Fill({ 0,255,0 });
+        test_bmp.Rotate(acos(-1) / 4);
+        test_bmp.Save("../test.bmp");
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
     return 0;
 }

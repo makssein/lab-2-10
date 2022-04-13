59bd7f8dbe2a4330c61941e3359d06091c062a7c
deekep <MVSein@stud.kantiana.ru>
2022-04-13 16:12:20 +0200

    add classes and lib

---
+++ inc/BMP.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Максим Сеин on 13.04.2022.
+//
+
+#ifndef LAB_2_10_BMP_H
+#define LAB_2_10_BMP_H
+
+#endif //LAB_2_10_BMP_H
---
+++ inc/mathutils/matrix.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Максим Сеин on 13.04.2022.
+//
+
+#ifndef LAB_2_10_MATRIX_H
+#define LAB_2_10_MATRIX_H
+
+#endif //LAB_2_10_MATRIX_H

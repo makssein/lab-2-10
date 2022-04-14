9b90b78cd9ac3ba77906f243d9f236d76454451e
deekep <MVSein@stud.kantiana.ru>
2022-04-14 10:58:54 +0200

    edit

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(){
         my::images::BMP test_bmp(1000, 1000);
         test_bmp.Open("../in.bmp");
         test_bmp.Rotate(acos(-1) / 2);
-        test_bmp.interpolation();
+        test_bmp.Interpolation();
         test_bmp.Save("../out.bmp");
     }
     catch (const std::exception& e){
